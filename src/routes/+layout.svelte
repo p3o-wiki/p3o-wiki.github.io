8760fefba9e1f0356e605ca8883acfc030108825
Madeline Sparkles <madeline@mouse.lgbt>
2024-03-25 10:51:03 +0800

    funny navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
 	import 'normalize.css'
+	import House from 'phosphor-svelte/lib/House'
+	import GearSix from 'phosphor-svelte/lib/GearSix'
 </script>
 
+<div class="navbar">
+	<a title="Home" href="/"><House weight="fill" /></a>
+	<a title="Settings" href="#settings"><GearSix weight="fill" /><span>(WIP)</span></a>
+</div>
 <slot />
